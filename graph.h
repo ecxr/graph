d5a3e4b945fe90923d6fec791f4f4c4a775e9a9d
ubuntu <foo@bar.com>
2013-11-11 16:20:42 +0000

    Add basic constructor

--- graph.h
+++ graph.h
@@ -3,12 +3,57 @@
 
 #include <iostream>
 #include <vector>
+#include <algorithm>
 
 using namespace std;
 
+//template <class NodeValue>
 class Graph
 {
+  typedef vector<vector<int>> EdgeList;
+  typedef vector<vector<double>> WeightList;
 
+  public:
+    Graph(int n) {
+      vCount = n;
+      eCount = 0;
+
+      adj.resize(n);
+      weights.resize(n);
+    }
+
+    void addEdge(int u, int v, double weight) {
+      // self-loops arent allowed
+      if (u == v) return;
+
+      int m = max(u,v);
+      if (m > vCount) {
+        adj.resize(m);
+        weights.resize(m);
+        vCount = m;
+      }
+
+      adj[u].push_back(v);
+      adj[v].push_back(u);
+
+      weights[u].push_back(weight);
+      weights[v].push_back(weight);
+
+      eCount++;
+    }
+
+    friend ostream& operator<<(ostream& out, Graph g)
+    {
+      cout << "(" << g.vCount << "," << g.eCount << ")";
+      return out;
+    }
+
+  private:
+    int vCount;
+    int eCount;
+    EdgeList adj;
+    WeightList weights;
+ //   vector<NodeValue> values;
 };
 
 #endif // _GRAPH_H
--- main.cpp
+++ main.cpp
@@ -5,7 +5,21 @@
 
 int main(int argc, char* argv[])
 {
+  cout << "initializing graph" << endl;
+  Graph g(10);
+
+  cout << "adding edges" << endl;
+  for (int i = 0; i < 10; i++) {
+    g.addEdge(i, (i + 3) % 10, (double)i);
+  }
+  cout << g << endl;
+/*
+  cout << "reading graph from a file";
+  Graph g2("testgraph.txt");
+  cout << g2 << endl;
+
   cout << "main" << endl;
   return 0;
+  */
 }
 
